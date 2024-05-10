175ce8b486adabde926833733a65771a505df402
Caique Ribeiro Rodrigues <ribeiro.caique95@gmail.com>
2024-05-10 14:22:50 -0300

    feature: create function to join the raffle as a player

--- src/Raffle.sol
+++ src/Raffle.sol
@@ -9,13 +9,28 @@ pragma solidity ^0.8.19;
  * @dev Implements Chainlink VRFv2
  */
 contract Raffle {
+    error Raffle__NotEnoughEth();
     uint256 private immutable i_entranceFee;
+    address payable[] private s_players;
+    // @dev Duration of the lottery in seconds
+    uint256 private immutable i_interval;
+    uint256 private s_lastTimestamp;
 
-    constructor(uint256 entranceFee) {
+    event EnteredRaffle(address indexed user);
+
+    constructor(uint256 entranceFee, uint256 interval) {
         i_entranceFee = entranceFee;
+        i_interval = interval;
+        s_lastTimestamp = block.timestamp;
     }
 
-    function enterRaffle() public payable {}
+    function enterRaffle() external payable {
+        if (msg.value < i_entranceFee) {
+            revert Raffle__NotEnoughEth();
+        }
+        s_players.push(payable(msg.sender));
+        emit EnteredRaffle(msg.sender);
+    }
 
     function pickWinner() public {}
 
